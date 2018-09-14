ab2a63f63ab7a0d0a5c2441fbb493bd97f0555ae
Radoslaw Wrobel <radoslaw1994@gmail.com>
2018-09-14 22:49:27 +0200

    Alpha version

---
+++ script/scroll.js
@@ -0,0 +1,35 @@
+jQuery(function ($)
+{
+    //scroll reset
+    $.scrollTo(0);
+    
+    $('#link_start').click(function () {
+        $.scrollTo($('#start'), 1000);
+    });
+    
+    $('#link_nas').click(function () {
+        $.scrollTo($('#o-nas'), 1000);
+    });
+    
+    $('#link_oferta').click(function () {
+        $.scrollTo($('#oferta'), 1000);
+    });
+    
+    $('#link_kontakt').click(function () {
+        $.scrollTo($('#kontakt'), 1000);
+    });    
+    
+    $('.scrollup').click(function () {
+        $.scrollTo($('body'), 1000);
+    });
+
+    //show scrollup button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300)
+            $('.scrollup').fadeIn();
+        else
+            $('.scrollup').fadeOut();
+    }
+    );
+}
+);
